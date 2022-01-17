f90d18d3f3738f1a69f5fcd56bd08b8e8721fce6
thasup <thanachonfirst@hotmail.com>
2022-01-18 02:33:20 +0700

    build Map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,31 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px");
+
+    const coordinates = { lat: 13.75, lng: 100.51 };
+
     return (
-        <div>
-            <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+                }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={15}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            ></GoogleMapReact>
         </div>
     );
 };

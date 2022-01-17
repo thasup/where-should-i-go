5ad5df39217b9f5c29184662a3fd2242cd936128
thasup <thanachonfirst@hotmail.com>
2022-01-18 01:56:45 +0700

    Add basic components

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ const App = () => {
                 <Grid item xs={12} md={4}>
                     <List />
                 </Grid>
-                <Grid item xs={12} md={8}>
+                <Grid
+                    item
+                    xs={12}
+                    md={8}
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
                     <Map />
                 </Grid>
             </Grid>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from "./styles";
+
+const Header = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Where Should I Go?
+                </Typography>
+                <Box className={classes.box}>
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search..."
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;

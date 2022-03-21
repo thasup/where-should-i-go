79716299633a242756ddfbaebbdb27c33ef2245c
thasup <thanachonfirst@hotmail.com>
2022-03-21 18:46:22 +0700

    Feat: search box

--- src/App.js
+++ src/App.js
@@ -8,83 +8,91 @@ import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 const App = () => {
-    const [places, setPlaces] = useState([]);
-    const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState({});
-    const [childClicked, setChildClicked] = useState(null);
-    const [type, setType] = useState("restaurants");
-    const [rating, setRating] = useState("");
-    const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({ lat: 13.75, lng: 100.51 });
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("all");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [autocomplete, setAutocomplete] = useState(null);
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            ({ coords: { latitude, longitude } }) => {
-                setCoordinates({ lat: latitude, lng: longitude });
-            }
-        );
-    }, []);
-
-    useEffect(() => {
-        if (bounds) {
-            setIsLoading(true);
-
-            getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
-                setPlaces(
-                    data.filter((place) => place.name && place.num_reviews > 0)
-                );
-                setFilteredPlaces([]);
-                setRating("");
-                setIsLoading(false);
-            });
-        }
-    }, [type, bounds]);
-
-    useEffect(() => {
-        const filtered = places.filter(
-            (place) => Number(place.rating) > rating
-        );
-        setFilteredPlaces(filtered);
-    }, [places, rating]);
-
-    return (
-        <>
-            <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{ width: "100%" }}>
-                <Grid item xs={12} md={4}>
-                    <List
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        childClicked={childClicked}
-                        isLoading={isLoading}
-                        type={type}
-                        setType={setType}
-                        rating={rating}
-                        setRating={setRating}
-                    />
-                </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    md={8}
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Map
-                        coordinates={coordinates}
-                        setCoordinates={setCoordinates}
-                        setBounds={setBounds}
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        setChildClicked={setChildClicked}
-                    />
-                </Grid>
-            </Grid>
-        </>
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
     );
+  }, []);
+
+  useEffect(() => {
+    const filtered = places.filter((place) => Number(place.rating) > rating);
+    setFilteredPlaces(filtered);
+  }, [places, rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setRating("");
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
+
+  const onLoad = (autoC) => {
+    setAutocomplete(autoC);
+  };
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+  };
+
+  return (
+    <>
+      <CssBaseline />
+      <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Map
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import { Autocomplete } from "@react-google-maps/api";
 
 import useStyles from "./styles";
 
-const Header = () => {
+const Header = ({ onLoad, onPlaceChanged }) => {
   const classes = useStyles();
 
   return (
@@ -18,18 +18,20 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
             </div>
-            <InputBase
-              placeholder="Search..."
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
